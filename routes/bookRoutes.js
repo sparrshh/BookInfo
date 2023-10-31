38a790f70f4d5f2b95c2af54a4826e9ef02a773c
sparrshh <sparshg756@gmail.com>
2023-10-31 18:48:31 +0530

    final commit

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -5,8 +5,9 @@ const {getAllBooks} =  require('../controllers/bookcontroller');
 const {createBook} =  require('../controllers/bookcontroller');
 const {updateBook} =  require('../controllers/bookcontroller');
 const {deleteBook} =  require('../controllers/bookcontroller');
+const {getBookById} =  require('../controllers/bookcontroller');
 
 
 router.route('/book').get(getAllBooks).post(createBook)
-router.route('/book/:id').put(updateBook).delete(deleteBook)
+router.route('/book/:id').get(getBookById).put(updateBook).delete(deleteBook)
 module.exports = router;
